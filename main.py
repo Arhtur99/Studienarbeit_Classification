40e1dfbde33edac3774c9027c2bf8291991d4c84
FavoriddyCent <75173349+FavoriddyCent@users.noreply.github.com>
2020-12-03 22:03:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,48 +4,41 @@ import matplotlib.pyplot as plt
 from sklearn import preprocessing, neighbors
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
 
 training = pd.read_csv("training.csv")
-
-for x in training["MMRCurrentAuctionCleanPrice"]:
-    if x == "nan":
-        print(x)
 
 training.replace(np.nan, -99999, inplace=True)
 
 training.drop(["RefId"], 1, inplace=True)
 
-X = np.array(training.drop(["IsBadBuy", "PurchDate", "Auction", "Make", "Model", "Trim", "SubModel", "Color", "Transmission", "WheelTypeID", "WheelType", "Nationality", "Size", "TopThreeAmericanName", "PRIMEUNIT", "AUCGUART", "VNST", ], 1))  # inplace = True
+
+drop_list = []
+
+for x in training.columns:
+    if training[x].dtype == "object":
+        drop_list.append(x)
+
+
+drop_list.append("IsBadBuy")
+drop_list.append("WheelTypeID")
+print(drop_list)
+
+"""
+y = df["party"].values
+X = df.drop(["party"], axis=1).values
+"""
+
+X = np.array(training.drop(drop_list, 1))
 y = np.array(training["IsBadBuy"])
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-clf = neighbors.KNeighborsClassifier()
+clf = neighbors.KNeighborsClassifier(n_neighbors=100)
 clf.fit(X_train, y_train)
 
 accuracy = clf.score(X_test, y_test)
 print(accuracy)
+accuracy = clf.score(X_train, y_train)
+print(accuracy)
 
-for x in training["Auction"].iloc:
-    if x != "ADESA" and x != "OTHER" and x != "MANHEIM":
-        print(x)
-"""
-example_measures = np.array([4,2,1,1,5,6,5,4,3,2,2])
-prediction = clf.predict(example_measures)
-example_measures = example_measures.reshape(len(example_measures), -1)
-print(prediction)
-"""
-
-"""
-y = training["IsBadBuy"].values
-X = training.drop(["IsBadBuy"], axis=1).values
-
-knn = KNeighborsClassifier(6)
-
-knn.fit(X, y)
-
-y_pred = knn.predict(X)
-
-new_prediction = knn.predict(X)
-print("Prediction: {}".format(new_prediction))
-"""
