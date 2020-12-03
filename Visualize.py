a58a8ea2a07c8712f7b2dfd1409f1ec73fff5481
FavoriddyCent <75173349+FavoriddyCent@users.noreply.github.com>
2020-12-03 22:04:47 +0100

    Update Visualize.py

--- Visualize.py
+++ Visualize.py
@@ -7,21 +7,15 @@ from sklearn.model_selection import train_test_split
 
 training = pd.read_csv("training.csv")
 
-
-plt.scatter(x = training["WarrantyCost"], y = training["VehBCost"], c = training["IsBadBuy"], label = "IsBadBuy")
-plt.legend()
-plt.show()
-
+#####################################################
+# PLOT WARRANTY UND ODOMETER SCATTER
 groups = training.groupby('IsBadBuy')
 
 for name, group in groups:
-    plt.plot(group["KickDate"], group["VehOdo"], marker='o', linestyle='', label=name)
+    plt.plot(group["WarrantyCost"], group["VehOdo"], marker='o', linestyle='', label=name)
 
-plt.xlabel("VehicleAge")
+plt.xlabel("WarrantyCost")
 plt.ylabel("VehOdo")
 plt.legend()
 plt.show()
-
-for x in training.iloc:
-    if x["VehBCost"] > 40000:
-        print(x)
+###################################################
