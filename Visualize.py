6114ab4148f3e9185a3b73dc10d80d6562c02268
Arhtur99 <Arthur.solowjow@gmx.de>
2020-12-04 11:59:23 +0100

    Update Visualize.py
    Neues Plot für Kilometerstand Verhältnis

--- Visualize.py
+++ Visualize.py
@@ -20,7 +20,42 @@ plt.legend()
 plt.show()
 ###################################################
 
+#PLOT FÜR VehOdo und IsBadBuy jeweils in 6000 Meilen Schritten
+
+data_vehodo_isbadbuy = dataframefeatures[["VehOdo", "IsBadBuy"]]
+upper = 6000
+lower = 0
+y_val = []
+x_val = []
+while upper <= 120000:
+    data = data_vehodo_isbadbuy.loc[(data_vehodo_isbadbuy.VehOdo >= lower) & (data_vehodo_isbadbuy.VehOdo < upper)]
+    data_list = list(data.IsBadBuy.value_counts())
+    
+    if len(data_list) < 2:
+        y_val.append(data_list[0] / data_list[0])
+        
+    else:
+        if data_list[1] == data_list[0]:
+            y_val.append(0)
+        else:
+            y_val.append(data_list[1] / data_list[0])
+    
+    x_val.append(upper)   
+    lower = upper
+    upper += 6000
+    
+plt.plot(x_val, y_val)
+
+plt.xlabel("VehOdo")
+plt.ylabel("IsBadBuy")
+plt.show()
+
+
+###################################################
+
+
 #Next Plot
+
 
 
 ###################################################
